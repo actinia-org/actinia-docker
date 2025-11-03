5424c2763f39651752b97c0817750b36ed744fc5
ninsbl <stefan.blumentrath@gmx.de>
2025-11-03 09:28:15 +0100

    ESA SANP 13

--- actinia-ubuntu/snap/install.sh
+++ actinia-ubuntu/snap/install.sh
@@ -1,43 +1,34 @@
-#!/bin/bash
+#!/bin/sh
 
 # https://senbox.atlassian.net/wiki/spaces/SNAP/pages/30539778/Install+SNAP+on+the+command+line
 # https://senbox.atlassian.net/wiki/spaces/SNAP/pages/30539785/Update+SNAP+from+the+command+line
-
 # http://step.esa.int/main/download/snap-download/
-SNAPVER=7
-# avoid NullPointer exception during S-1 processing
-java_max_mem=10G
 
-# set JAVA_HOME (done in Docker as well)
-export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+# Download ESA SNAP installer
+wget -q --show-progress -O /src/snap/esa-snap_all_linux-${ESA_SNAP_VERSION}.0.0.sh \
+ "https://download.esa.int/step/snap/${ESA_SNAP_VERSION}.0/installers/esa-snap_all_linux-${ESA_SNAP_VERSION}.0.0.sh"
 
-# install module 'jpy' (A bi-directional Python-Java bridge)
-git clone https://github.com/bcdev/jpy.git /src/snap/jpy
-pip3 install wheel
-(cd /src/snap/jpy && python3 setup.py bdist_wheel)
-# hack because ./snappy-conf will create this dir but also needs *.whl files...
-mkdir -p /root/.snap/snap-python/snappy
-cp /src/snap/jpy/dist/*.whl "/root/.snap/snap-python/snappy"
+# Install ESA SNAP and plugins
+echo "Installing ESA SNAP ${ESA_SNAP_VERSION}.0.0 ..."
+sh /src/snap/esa-snap_all_linux-${ESA_SNAP_VERSION}.0.0.sh -q -varfile /src/snap/response.varfile
 
-# install and update snap
-wget -q -O /src/snap/esa-snap_all_unix_${SNAPVER}_0.sh \
-  "http://step.esa.int/downloads/${SNAPVER}.0/installers/esa-snap_all_unix_${SNAPVER}_0.sh"
-sh /src/snap/esa-snap_all_unix_${SNAPVER}_0.sh -q -varfile /src/snap/response.varfile
-/usr/local/snap/bin/snap --nosplash --nogui --modules --update-all
+sed -i 's+jdkhome="./jre"+jdkhome="$JAVA_HOME"+g' /usr/local/esa-snap/etc/snap.conf
 
-# create snappy and python binding with snappy
-/usr/local/snap/bin/snappy-conf /usr/bin/python3
-(cd /root/.snap/snap-python/snappy && python3 setup.py install)
+# Update ESA SNAP and plugins
+echo "Updating ESA SNAP ${ESA_SNAP_VERSION}.0.0 ..."
+# freezing, when no updates available. Now there are updates, so reactivating.
+/usr/local/esa-snap/bin/snap --nosplash --nogui --modules --update-all 2>&1 | while read -r line; do
+    echo "$line"
+    [ "$line" = "updates=0" ] &&
+    (ps -u
+    pgrep -f -a "/usr/local/esa-snap/bin"
+    echo ""
+    pgrep -f -a "snap")
+        [ "$line" = "updates=0" ] && sleep 2 && pkill -f "/usr/local/esa-snap/bin"
+done
 
-# increase the JAVA VM size to avoid NullPointer exception in Snappy during S-1 processing
-(cd /root/.snap/snap-python/snappy && sed -i "s/^java_max_mem:.*/java_max_mem: $java_max_mem/" snappy.ini)
-
-# get minor python version
-PYMINOR=$(python3 -c 'import platform; major, minor, patch = platform.python_version_tuple(); print(minor)')
-(cd /usr/local/lib/python3.$PYMINOR/dist-packages/snappy/ && sed -i "s/^java_max_mem:.*/java_max_mem: $java_max_mem/" snappy.ini)
-
-# test
-/usr/bin/python3 -c 'from snappy import ProductIO'
-
-# cleanup installer
-rm -f /src/snap/esa-snap_all_unix_${SNAPVER}_0.sh
+# Cleanup
+rm -rf /usr/local/esa-snap/jre \
+rm -rf /src/snap
+# Maybe remove more unnecessary files from /root/.snap which is later copied to the final image
+echo "Done installing ESA SNAP ${ESA_SNAP_VERSION}.0.0 ..."
